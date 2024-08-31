eeae8d8a5b50b32cae94c637a4bf48786dc63178
Shueny Wang <shuen29@gmail.com>
2024-08-31 11:22:22 +0800

    chore: Update pnpm version and configuration files

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // <=== enables static exports
+  output: 'export', // Enables static exports
   reactStrictMode: true,
+  basePath: '/next-blog', // Replace with your repository name
+  assetPrefix: '/next-blog/', // Replace with your repository name
+  images: {
+    unoptimized: true,
+  },
 }
 
-module.exports = nextConfig
 export default nextConfig
