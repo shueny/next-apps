56b4e35c382aa104b5d4258b43a261560782a51a
Shueny Wang <shuen29@gmail.com>
2024-08-31 11:09:59 +0800

    chore: Update next.config.mjs and package.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export', // <=== enables static exports
+  reactStrictMode: true,
+}
 
-export default nextConfig;
+module.exports = nextConfig
+export default nextConfig
