70513227fa38f57667e5be2e84e85c7598849d58
shueny <shuen29@gmail.com>
2024-12-11 18:05:50 +0800

    fix: fs problem

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,12 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+
+    return config;
+  },
 }
+
 
 export default nextConfig
